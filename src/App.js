f9340aaf63fc1a1b1a29815dc3a69b72e1f41b04
RiyaChippy <riyashajim@gmail.com>
2024-10-22 02:27:00 +0530

    Changes made at 2:30 AM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import img1 from './rocket.jpg';
+import ParticleBg from './components/ParticleBg';
 
+// Removed unused imports: Routes, Route, Link, NextPage
+
+// Define the App component
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ParticleBg />
+
+      <div className="App">
+        <header className="App-header">
+          <p className="text">Hover over the rocket fire to launch it</p>
+          <img
+            src={img1}
+            alt=" "
+            className={`rocket ${isHovered ? 'hovered' : ''}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+
+          {isHovered && (
+            <div className="centered-content">
+              <p className="hover-text">Hello Everyone!</p>
+            </div>
+          )}
+        </header>
+      </div>
+    </>
   );
 }
 
+// Export the App component as the default export
 export default App;
